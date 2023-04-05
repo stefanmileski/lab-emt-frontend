97de984daddbe2730c033ba9e9000b064de6981f
Stefan Mileski <stefanmileski@yahoo.com>
2023-04-05 04:06:57 +0200

    set up axios and api calls

---
+++ src/api-calls/api-calls.js
@@ -0,0 +1,37 @@
+import { async } from "q";
+import instance from "../axios/axios";
+
+export async function getAllBooks() {
+  const response = await instance.get("/books");
+  return response.data;
+}
+
+export async function getBookById(id) {
+  const response = await instance.get(`/books/${id}`);
+  return response.data;
+}
+
+export async function addBook(book) {
+  const response = await instance.post("/books", book);
+  return response.data;
+}
+
+export async function updateBook(book) { 
+  const response = await instance.put(`/books/${book.id}`, book);
+  return response.data;
+}
+
+export async function deleteBook(id) {
+  const response = await instance.delete(`/books/${id}`);
+  return response.data;
+}
+
+export async function markAsRented(id) {
+  const response = await instance.put(`/books/${id}/rent`);
+  return response.data;
+}
+
+export async function getAllBooksWithPagination(pageNumber, pageSize) {
+  const response = await instance.get(`/books?page=${pageNumber}&size=${pageSize}`);
+  return response.data;
+}
